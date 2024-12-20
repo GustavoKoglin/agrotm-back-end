d3acf3f0fa769b7f9283b7a77e7cd2e3961f1ca1
GustavoKoglin <engcomputacao.gustavokoglin@gmail.com>
2024-12-19 22:09:39 -0400

    Atualização de Domains

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
-      "https://agrotm-devmichaelhemings-projects.vercel.app",
-      "https://agrotm-dashboard.vercel.app",
+      "https://agrotm.vercel.app",
+      "https://agrotm-dashboard-seven.vercel.app",
       "http://localhost:3000",
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
